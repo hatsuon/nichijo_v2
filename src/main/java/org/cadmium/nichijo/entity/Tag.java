d0fdfb173ae624f40924f51b6bdd92f861c8e38f
xiangkunxu <solitudi@icloud.com>
2022-06-26 10:17:53 +0800

    User 实体

---
+++ src/main/java/org/cadmium/nichijo/entity/Tag.java
@@ -0,0 +1,19 @@
+package org.cadmium.nichijo.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.cadmium.nichijo.common.model.BasicEntity;
+
+import java.io.Serial;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Tag extends BasicEntity {
+
+    @Serial
+    private static final long serialVersionUID = -3470688086625110266L;
+
+    private String name;
+    private Integer userId;
+
+}
---
+++ src/main/java/org/cadmium/nichijo/entity/User.java
@@ -0,0 +1,25 @@
+package org.cadmium.nichijo.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.cadmium.nichijo.common.model.BasicEntity;
+
+import java.io.Serial;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class User extends BasicEntity {
+
+    @Serial
+    private static final long serialVersionUID = 7012525950948954722L;
+
+    private Integer id;
+
+    private Boolean type;
+
+    private String avatar;
+    private String username;
+    private String nickname;
+    private String password;
+
+}
